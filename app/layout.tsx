18632c056c9dfda19accd5380606435819160eb3
Stephanie Sithu <SESITH01@louisville.edu>
2026-02-09 01:39:11 -0500

    added first envelope button transisiton between open and close

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/ros4cmc.css" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
