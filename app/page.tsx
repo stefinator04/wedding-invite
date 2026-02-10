f2e09432cda49680d6b9bcbd2d2e2b19430e1290
Stephanie Sithu <SESITH01@louisville.edu>
2026-02-09 22:27:44 -0500

    added invite, location, large-med polaroids, mandala, and tag to site

--- app/page.tsx
+++ app/page.tsx
@@ -48,6 +48,42 @@ export default function Home() {
             CLICK TO OPEN
           </p>
         )}
+        <img
+          src="/images/invite.png"
+          alt="Invitation"
+          style={{ transitionDelay: isOpen ? "300ms" : "0ms" }}
+          className={`absolute left-[6%] top-[75%] w-[57%] transition-all duration-700 ease-in-out ${isOpen ? "opacity-100" : "pointer-events-none translate-y-4 opacity-0"}`}
+        />
+        <img
+          src="/images/location.png"
+          alt="Location"
+          style={{ transitionDelay: isOpen ? "200ms" : "0ms" }}
+          className={`absolute right-[6%] top-[63%] z-10 w-[56%] transition-all duration-700 ease-in-out ${isOpen ? "opacity-100" : "pointer-events-none translate-y-4 opacity-0"}`}
+        />
+        <img
+          src="/images/mandala.png"
+          alt="Mandala"
+          style={{ transitionDelay: isOpen ? "400ms" : "0ms" }}
+          className={`absolute right-[7%] top-[115%] z-[5] w-[52%] transition-all duration-700 ease-in-out ${isOpen ? "opacity-100" : "pointer-events-none translate-y-4 opacity-0"}`}
+        />
+        <img
+          src="/images/polaroid-large.png"
+          alt="Polaroid large"
+          style={{ transitionDelay: isOpen ? "500ms" : "0ms" }}
+          className={`absolute left-[13%] top-[132%] z-[4] w-[39%] -rotate-[14deg] transition-all duration-700 ease-in-out ${isOpen ? "opacity-100" : "pointer-events-none translate-y-4 opacity-0"}`}
+        />
+        <img
+          src="/images/polaroid-medium.png"
+          alt="Polaroid medium"
+          style={{ transitionDelay: isOpen ? "600ms" : "0ms" }}
+          className={`absolute right-[13%] top-[161%] z-[4] w-[42%] rotate-[15deg] transition-all duration-700 ease-in-out ${isOpen ? "opacity-100" : "pointer-events-none translate-y-4 opacity-0"}`}
+        />
+        <img
+          src="/images/tag.png"
+          alt="Tag"
+          style={{ transitionDelay: isOpen ? "700ms" : "0ms" }}
+          className={`absolute left-[10%] top-[157%] z-[4] w-[57%] transition-all duration-700 ease-in-out ${isOpen ? "opacity-100" : "pointer-events-none translate-y-4 opacity-0"}`}
+        />
       </div>
     </div>
   );
